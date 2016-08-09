5c8a146a5eb8f1ae7465af333995dfef8a1ca436
Sam Chesney <samc@xmos.com>
2016-08-09 08:59:22 +0100

    Update copyright with first and latest publication dates

--- lib_filesystem/api/qspi_flash_storage_media.h
+++ lib_filesystem/api/qspi_flash_storage_media.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2016, XMOS Ltd, All rights reserved
+// Copyright (c) 2015-2016, XMOS Ltd, All rights reserved
 #ifndef __qspi_flash_storage_media_h__
 #define __qspi_flash_storage_media_h__
 
